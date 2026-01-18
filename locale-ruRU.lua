db92abc4e584abf66539f87659abc1b29b2202ff
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-01-18 13:27:44 +0300

    Add files via upload

---
+++ locale-ruRU.lua
@@ -0,0 +1,22 @@
+if GetLocale() == "ruRU" then
+  local addonName, addonTable = ...
+  local L = addonTable.L
+-- Translator ZamestoTV
+L["ATT_MINIMAP"]              = "Прикрепить к миникарте"
+L["BY"]                       = "от"
+L["COMMING_SOON"]             = "Скоро:"
+L["DATES"]                    = "Дата"
+L["DAY"]                      = "день"
+L["DAYS"]                     = "дней"
+L["ENDS"]                     = "заканчивается"
+L["NEW_EVENT"]                = "Новое событие"
+L["NO_EVENTS_TODAY"]          = "Сегодня событий нет"
+L["STARTS"]                   = "начинается"
+L["TIMES"]                    = "Время"
+L["TODAY"]                    = "Сегодня:"
+L["DATE_FORMAT"]              = "Формат даты"
+L["DATE_FORMAT_DESC"]         = "Символы D, M, Y будут заменены на день, месяц, год."
+L["DATE_FORMAT_WITHOUT_YEAR"] = "Формат даты без года"
+L["DATE_FORMAT_WITHOUT_YEAR_DESC"] = "Символы D, M будут заменены на день и месяц."
+L["DESCRIPTION"]              = "|cffeda55fЛКМ|r — открыть календарь,\n|cffeda55fПКМ|r — открыть настройки аддона."
+end
